45dabf2452bf662f86182304b2c0a2a778a30c0b
Joel Burton <joel@joelburton.com>
2025-01-19 15:34:03 -0800

    Set

--- bridge_theme/__init__.py
+++ bridge_theme/__init__.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+from docutils import nodes
+from sphinx.util.docutils import SphinxRole
+
+class DiscreetRole(SphinxRole):
+    def run(self) -> tuple[list[nodes.Node], list[nodes.system_message]]:
+        return [nodes.emphasis(text=self.text, classes=['discreet'])], []
 
 def setup(app):
     app.add_html_theme('bridge_theme', Path(__file__).resolve().parent)
+    app.add_role('discreet', DiscreetRole())
